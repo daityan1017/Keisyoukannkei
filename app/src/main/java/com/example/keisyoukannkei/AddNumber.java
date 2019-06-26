0bce812e375c1ae649258b56e35749013a921605
daityan1017 <nettouroku.daityan@Gmail.com>
2019-06-26 21:44:45 +0900

    first

---
+++ app/src/main/java/com/example/keisyoukannkei/AddNumber.java
@@ -0,0 +1,13 @@
+package com.example.keisyoukannkei;
+
+public class AddNumber {
+    protected int number ;
+
+    public void addNumber(int i){
+        number += i;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
